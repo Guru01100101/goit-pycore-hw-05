d9f3bb60575c294f91e9423a385de80cdfebfc72
Bohdan Horobets <91347979+Guru01100101@users.noreply.github.com>
2024-07-21 15:07:03 +0300

    Ex2 generator_numbers solved.

--- generator_numbers.py
+++ generator_numbers.py
@@ -18,10 +18,20 @@ def generator_numbers(text: str) -> Callable:
     return generator
 
 
+def sum_profit(text: str, generator: Callable) -> float:
+    """
+    Returns the sum of the numbers generated from the text using the generator
+    :arg text: the text to generate the numbers from (separated by spaces)
+    :arg generator: the generator function to generate the numbers
+    :return: the sum of the numbers
+    """
+    gen = generator(text)
+    return sum(gen())
+
+
 if __name__ == '__main__':
     text = ("Загальний дохід працівника складається з декількох частин: "
             "1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і "
             "324.00 доларів.")
-    gen = generator_numbers(text)
-    for number in gen():
-        print(number)
+    total_income = sum_profit(text, generator_numbers)
+    print(f"Загальний дохід: {total_income}")  # Загальний дохід: 1351.46
