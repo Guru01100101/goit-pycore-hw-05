9b7bef8d6ec7b45757abf4b350545a06fbc683ba
Bohdan Horobets <91347979+Guru01100101@users.noreply.github.com>
2024-07-21 15:28:26 +0300

    Ex3 log_parser: parse_log_line complete (not tested yet)

--- log_parser.py
+++ log_parser.py
@@ -1,16 +1,26 @@
 import re
-from typing import List, Dict
+from typing import List, Dict, Pattern
 from pathlib import Path
 
-
 # pattern for log lines in the format: "2021-01-01 12:00:00 INFO Message"
-log_pattern = re.compile(r'(?P<timestamp>\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}) '
-                         r'(?P<level>\w+) '
-                         r'(?P<message>.*)')
+log_format_pattern = re.compile(r'(?P<timestamp>\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}) '
+                                r'(?P<level>\w+) '
+                                r'(?P<message>.*)')
 
 
-def parse_log_line(line: str) -> dict:
-    ...
+def parse_log_line(line: str, log_pattern: Pattern[str] = log_format_pattern) -> dict:
+    """
+    Parses a log line and returns a dictionary with the timestamp, level, and message
+    :arg line: log line to parse
+    :arg log_pattern: compiled regular expression pattern for log lines
+    :return: dictionary with the timestamp, level, and message
+    :err return: empty dictionary if the line does not match the pattern
+    """
+
+    match = log_pattern.match(line)
+    if match:
+        return match.groupdict()
+    return {}
 
 
 def load_logs(logfile_path: str) -> list:
